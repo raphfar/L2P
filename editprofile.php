8a458b3e7ba7babdd042e83ad81d2b0bfbf5a58a
raphfar <raphael.farina@devinci.fr>
2017-05-19 16:27:31 +0200

    Create editprofile.php

---
+++ editprofile.php
@@ -0,0 +1,40 @@
+<?php 
+  session_start(); 
+  require("conf/db.php");
+
+
+    if (isset($_POST['email']) && !empty($_POST['email'])
+    && isset($_POST['pseudo']) && !empty($_POST['pseudo'])) {
+
+      $email   = htmlspecialchars($_POST["email"]);
+      $pseudo = htmlspecialchars($_POST["pseudo"]);
+
+        $request = $db->prepare('INSERT INTO posts (email, pseudo, password, elo, role) 
+          VALUES (:email, :pseudo, :elo, :role, NOW(), :user_id)');
+
+        $request->execute(
+          array(
+            'email' => $email,
+            'pseudo' => $pseudo,
+            'elo' => $elo,
+            'role' => $role,
+            'user_id' => $_SESSION['user_id']
+          )
+        );
+
+echo "merci de votre contribution.";
+      }
+?>
+
+  <form action="user.php" method="post">
+    <fieldset>
+      <legend><h4>Formulaire de contact</h4></legend>
+      <br>
+        <input id="email" name="email" type="text" placeholder=" votre email"><br><br>               
+        <textarea id="pseudo" name="pseudo" placeholder="votre pseudo"></textarea><br><br>
+        <textarea id="elo" name="elo" placeholder="votre elo"></textarea><br><br>
+        <textarea id="role" name="role" placeholder="votre role"></textarea><br><br>
+        <input type="submit" value="Envoyer"/>
+    </fieldset>
+  </form>
+<a href='index.php'>home</a>
