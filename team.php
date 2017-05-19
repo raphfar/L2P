f8afc62301886661d8a8c58d9b60440e899228ca
raphfar <raphael.farina@devinci.fr>
2017-05-19 15:56:33 +0200

    Create team.php

---
+++ team.php
@@ -0,0 +1,22 @@
+<?php  
+
+
+    $request2 = $db->query('SELECT user_id, team_name, elo, line_up, path FROM Team');
+
+    while ($data = $request2->fetch()) {
+     
+?>
+   <div class="row">
+        <div class=" col-md-3">
+        
+    <p><?php echo $data['team_name']; ?></p>
+
+    <p><?php echo $data['elo']; ?></p>
+
+    <p><?php echo $data['line_up']; ?></p>
+
+    
+            </div>
+        </div>
+        
+ <?php  } ?>
