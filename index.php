029106efc7f39f49cced1e9b3a95facd58b591cb
greg91700 <gregoire.durussel@devinci.fr>
2017-05-26 15:48:28 +0200

    explication sur l'index

--- index.php
+++ index.php
@@ -9,8 +9,13 @@ include("head.php");
 <a href="login.php">se connecter</a> <br>
 <a href="logout.php">se déconnecter</a> <br>
 <a href="joueurs.php">recherche de joueur</a> <br>
-<a href="">recherche de team</a> <br>
-<a href="">modifier ses information</a> 
+<a href="teams.php">recherche de team</a> <br>
+<a href="">modifier ses information</a>
+<br><br>
+<h1>Explication du site : grace a cette merde vous pouvez trouver de 
+	nombreux joueurs de tous niveaux il vous suffit d'entrer en contact avec la
+personne concernée via son adresse email ou via son pseudo sur league of legend directement.
+Nous espérons que vous trouverez des coéquipiés adaptés :)</h1>
 
 </body>
 </html>
